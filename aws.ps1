28fb6cf98a9e240a5049fe882cf3050617d51140
prakash4600 <114327234+prakash4600@users.noreply.github.com>
2023-02-14 17:25:21 +0530

    Update aws.ps1

--- aws.ps1
+++ aws.ps1
@@ -1,1 +1,1 @@
-aws s3api create-bucket     --bucket bhanuprakash460     --region eu-west-1     --create-bucket-configuration LocationConstraint=eu-west-1
+aws s3api create-bucket     --bucket bhanuprakash46450     --region eu-west-1     --create-bucket-configuration LocationConstraint=eu-west-1
